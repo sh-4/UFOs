52f8bab2f1eb5624b81b7466f33f7d2fd189ef58
sh-4 <105808695+sh-4@users.noreply.github.com>
2022-08-18 14:18:14 -0400

    updated: show table data and have filter function

--- static/js/app.js
+++ static/js/app.js
@@ -31,3 +31,27 @@ function buildTable(data) {
         );
     });
     }
+
+// add function to filter table data
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+
+    // set a default filter to the original table data because we want users to refine their search on their own terms
+    let filteredData = tableData;
+
+    // create an if statement to check if a date was entered and filter the data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+        };
+    // Rebuild the table using the filtered data
+    // NOTE: If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
